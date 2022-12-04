c3b017db9a4b1dc196ff8f1ddbc4901c97f4ca36
+ <+>
2022-12-04 13:40:00 +0300

    Updates to index.js for form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+//for the input fields
+const newTaskForm = document.getElementById("create-task-form");
+const newTaskDescription = document.getElementById("new-task-description");
+const newTaskPriority = document.getElementById("new-task-priority");
+
+//unordered list where new tasks will stay on the DOM
+const newTaskUl = document.getElementById("tasks");
+
+//attaching an event listener:
+newTaskForm.addEventListener("submit", createNewTask);
 });
+
+const createNewTask = event => {
+event.preventDefault();
+
+//to stop the form fro trying to submit:
+const newTaskDescription = document.getElementById("new-task-description");
+const newTask = document.createElement("li");
+newTask.innerText = newTaskDescription.value;
+
+appendNewTask(newTask);
+event.target.reset();
+};
+
+const appendNewTask = task => {
+document.getElementById("tasks").appendChild(task);
+};
